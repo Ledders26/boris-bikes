71ac57f463192b6d4d7bfe945ed756307b8468df
Ledders26 <benlhall584@hotmail.co.uk>
2022-01-25 16:38:31 +0000

    Added DockingStation class to boris_bikes.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
 require_relative "../lib/boris_bikes.rb"
 
 describe DockingStation do
-    describe "#release" do
-        it "should release the bike for the person" do
-            docking_station = DockingStation.new
-            expect(docking_station.release).to eq "Released!"
-        end
+    describe "#release_bike" do
+       it "should release the bike for the person" do
+           docking_station = DockingStation.new
+           expect(docking_station.release_bike).to eq "Released!"
+       end
     end
 end
