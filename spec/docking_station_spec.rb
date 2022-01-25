9b16216f038a98fa17f2b823352a6285be6e518d
Ledders26 <benlhall584@hotmail.co.uk>
2022-01-25 17:04:53 +0000

    written a 'it' test to check for release bike and then added release bike method to docking_station.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
     describe "#release_bike" do
        it "should release the bike for the person" do
            docking_station = DockingStation.new
-           expect(docking_station.release_bike).to eq "Released!"
+           expect(docking_station).to respond_to(:release_bike).with(1).argument
        end
     end
 end
